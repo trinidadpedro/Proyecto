141fe1c6d0de0612e3794ccb66a8c560402290cc
trinidadpedro <43254814+trinidadpedro@users.noreply.github.com>
2018-09-14 19:43:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/acer/proyecto/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.acer.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+//public void GenerarRFC(View v){
+
+   //EditText edApPaterno=(EditText)findViewById(R.id.edtAPPaterno);
+   //         Char primerLetra=caAux.charAt(0);
+          //  charn primerLetraAP =edApPaterno.getText().charAt(0);
+
+           // char [] vocales={'A','E','I','O','U'};
+          //  String cadAux="Pedro Perez";
+
+
+
+
+//}
+
+
+
+//Tomar la primera letra y la primera vocal del apellido paterno
+//Tomar  la primer letra del apellido materno
+//Tomar la primer letra del primer nombre
+//Tomar lso ultimos  digitos del primer año  de nacimiento
+//Tomar los dos digitos del mes de nacimiento
+//Tomar los dos digitos del dia  de nacimiento
+//Generar 3 caracteristicas(letras mayusculas o numeros)
